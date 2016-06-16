c52e32e289331b92a771b838377fe0ed6d63a38a
JazQ <jade1921@gmail.com>
2016-06-16 16:13:16 +0800

    记录二进制位数上1出现的次数，对3取余，则为single number的相应位数上的数

---
+++ Single-Number-II.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int singleNumber(vector<int> &nums) {
+    vector<int> bit(32);
+    int result = 0;
+    for (int i = 0; i < nums.size(); i++) {
+      for (int j = 0; j < 32; j++) {
+        bit[j] += (nums[i] >> j) & 1;
+        bit[j] %= 3;
+      }
+    }
+    for (int i = 0; i < 32; i++)
+      result += bit[i] << i;
+    return result;
+  }
+};
+
+int main() {
+  vector<int> nums{1, 1, 1, 2, 2, 2, 3, 4, 4, 4};
+  Solution s;
+  cout << s.singleNumber(nums) << endl;
+  ;
+}
